66777531328ea0c5d807c2215678daf313632531
Binesh Pradhananga <binesh@vianet.com.np>
2024-06-26 15:28:46 +0545

    Initial commit for YourPackage

---
+++ src/YourPackageServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace YourVendor\YourPackage;
+
+use Illuminate\Support\ServiceProvider;
+
+class YourPackageServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // You can publish config, views, migrations, etc. here
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Register package services here
+    }
+}
